cc40c2a4c56b5b6edc1a74fc3c17c88c8006124b
kailahk <kailahkorsh@me.com>
2023-01-24 15:33:59 -0800

    implements 404 error page for all routes that do not match the routes outlined in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Home';
 import Create from './Create';
 import { Routes, Route } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 export default function App() {
   const title = 'Blog for Fun'
@@ -14,6 +15,7 @@ export default function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
             <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
