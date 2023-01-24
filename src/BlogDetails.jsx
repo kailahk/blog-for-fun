5ed357d0a793187b9b617dea4827d4d6315a19a4
kailahk <kailahkorsh@me.com>
2023-01-24 14:41:14 -0800

    blog form creation and styling

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom"
+import useFetch from "./useFetch";
 
 export default function BlogDetails() {
     const { id } = useParams();
+    const { data: blog, error, luckyNum } = useFetch(`http://localhost:8000/blogs/${id}`)
+
     return (
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            {error && <div>{ error }</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.content}</div>
+                </article>
+            )}
         </div>
     )
 }
--- src/Create.jsx
+++ src/Create.jsx
@@ -2,6 +2,23 @@ export default function Create() {
     return (
         <div className="create">
             <h2>Add a New Blog Post</h2>
+            <form>
+                    <label>Title:</label>
+                    <input
+                        type="text"
+                        required
+                    />
+                    <label>Body:</label>
+                    <textarea
+                        required
+                    >
+                    </textarea>
+                    <label>Author:</label>
+                    <input
+                        type="text"
+                        required
+                    />
+            </form>
         </div>
     )
 }
